2dcddd016b073456e115cc14a0f06226f939814d
AJ Vrieland <zer0relm@gmail.com>
2020-02-24 15:30:32 -0500

    Project03 Ada

--- Projects/03/Ada/grade_book.adb
+++ Projects/03/Ada/grade_book.adb
@@ -1,17 +1,23 @@
-
-
-
+-- grade_book.adb converts numeric sores to letter grades
+--
+-- Completed by: AJ Vrieland (ajv234)
+-- Date: 02/24/2020
+--
+-- Input: grade_num, a String
+-- Precondition: grade_num is a number from 1-100
+-- Output: The equivalent letter grade
+--------------------------------------
 
 with Ada.Text_IO, Ada.Integer_Text_IO;
 use  Ada.Text_IO, Ada.Integer_Text_IO;
 
 procedure grade_book is
 
-    grade_num : string;
-    grade : integer;
+    grade_num : String(1..3) := "   ";
+    grade : integer := 1;
     charsRead : Natural;
 
-    function gradeBook(grade: in integer) return string is
+    function gradeBook(grade_num: in integer) return string is
     begin
       grade := grade / 10;
       case grade is
@@ -21,12 +27,15 @@ procedure grade_book is
         when 6 => return "D";
         when others => return "F";
       end case;
-    end
+    end gradeBook;
 
 begin
-    Put("Enter grade: ");
-    Get_Line(grade, charsRead);
+  while grade_exit /= 0 loop
+    Put("Enter grade (Enter 0 to exit): ");
+    Get_Line(grade_num, charsRead);
     grade := Integer'Value (grade_num);
     Put( gradeBook(grade));
+    New_Line;
+  end loop;
 
 end grade_book;
