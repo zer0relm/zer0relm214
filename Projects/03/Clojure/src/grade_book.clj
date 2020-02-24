d71d6a7808d59c9ece45a27dbc06a23519410de4
AJ Vrieland <zer0relm@gmail.com>
2020-02-24 17:16:42 -0500

    Project03 clojure (no comments)

--- Projects/03/Clojure/src/grade_book.clj
+++ Projects/03/Clojure/src/grade_book.clj
@@ -9,23 +9,19 @@
     (= grade 10) "A"
     (= grade 9) "A"
     (= grade 8) "B"
+    (= grade 7) "C"
+    (= grade 6) "D"
+    :else "F"
     )
 )
 
+
 (defn -main []
   (def grade 1)
-  (while (not= grade 0)
-    (do
-      (print "\nEnter your Grade: ") (flush)
+      (print "\nEnter your grade: ") (flush)
       (let
-        [grade_str (read-line)]
-        ;(set grade (Integer.grade_str))
-        (set grade (read-string grade_str))
-        (Integer. grade)
-        (set grade (/ grade 10))
-        (println (gradeBook grade))
+        [grade (Integer/parseInt (read-line))]
+        (def score (quot grade 10))
+        (println (gradeBook score))
       )
-    )
-  )
-
 )
