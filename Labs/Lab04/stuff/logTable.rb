89bfd112d286d1faa6a679701755bd72f6e69f90
AJ Vrieland <zer0relm@gmail.com>
2020-02-25 12:33:07 -0500

    Lab04 start

---
+++ Labs/Lab04/stuff/logTable.rb
@@ -0,0 +1,24 @@
+#! /usr/bin/ruby
+# logTable.rb displays a table of logarithms
+#
+# Begun by: Prof. Adams, for CS 214 at Calvin College.
+# Completed by:
+#
+# Input:  The start, stop and increment values
+# Precondition: The start value is less than the stop value, 
+#     and the increment is greater than 0
+# Output: A table of logarithms from start to stop, with increment
+#     as the step value
+
+
+if __FILE__ == $0
+   print "Enter the start value: "
+#### ADD CODE HERE TO GET THESE VALUES AS NUMBERS
+   print "Enter the stop value: "
+####
+   print "Enter the increment value: "
+####   
+   
+#  Replace this line with a loop to display the log table 
+end
+
---
+++ Labs/Lab04/stuff/log_table.adb
@@ -0,0 +1,27 @@
+-- log_table.adb computes a table of logarithms.
+--
+-- Input: start, stop, increment, three reals.
+-- Precondition: increment is positive.
+-- Output: A table of logarithms beginning with log(start),
+--         ending with log(stop), with intervals of increment.
+--
+-- Begun by: Prof. Adams, for CS 214 at Calvin College.
+-- Completed by:
+--------------------------------------------------------------
+
+with Ada.Text_IO, Ada.Float_Text_IO;
+use  Ada.Text_IO, Ada.Float_Text_IO;
+
+procedure log_table is
+
+   Start, Stop, Increment : Float;
+
+begin                                           -- Prompt for input
+   Put_Line("To print a table of logarithms,");
+   Put(" enter the start, stop, and increment values: ");
+   Get(Start); Get(Stop); Get(Increment);
+
+   -- Replace this line with a loop to generate the table of logs.
+
+end log_table;
+
