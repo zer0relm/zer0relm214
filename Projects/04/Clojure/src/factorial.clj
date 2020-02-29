40c732ac429f9dc4c0eeb5037923ed90c6f5a392
AJ Vrieland <zer0relm@gmail.com>
2020-02-29 10:08:35 -0500

    Project04 Clojure no comments

---
+++ Projects/04/Clojure/src/factorial.clj
@@ -0,0 +1,18 @@
+
+(ns factorial)
+
+
+(defn -main []
+  (print "Enter Factorial: ") (flush)
+  (let
+    [ number (double (read)) ]
+    (def answer 1)
+    (loop [i 2]
+      (when (<= i number)
+        (def answer (* answer i))
+        (recur (+ i 1))
+      )
+    )
+    (println answer)
+  )
+)
