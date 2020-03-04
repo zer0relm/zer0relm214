6df5fc7ad4dc2f4529d857ff9681c705942a255c
AJ Vrieland <zer0relm@gmail.com>
2020-03-04 12:28:01 -0500

    Lab05 start

--- Labs/Lab05/05/Java/Split.java
+++ Labs/Lab05/05/Java/Split.java
@@ -1,8 +1,8 @@
 /* Split.java is a class and program that splits a string
  * into two substrings.
- * 
+ *
  * Begun by: Serita Nelesen
- * Completed by: 
+ * Completed by:
  ***************************************************************/
 
 import java.util.Scanner;
@@ -17,7 +17,7 @@ public class Split
       // prompt for the string
       System.out.println("\nTo split a string, enter the string: ");
       String theString = keyboard.next();
-      
+
       // prompt for splitting position
       System.out.println("\nEnter the split position: ");
       int position = keyboard.nextInt();
@@ -26,15 +26,17 @@ public class Split
       String [] resultList = new String[2];
       splitter(theString, position, resultList);
       System.out.println("\nThe first part is " + resultList[0] + "\n and the second part is " + resultList[1]);
-      
+
     }
-  
+
   /* splitter() splits a string according to the position specified
    * Receive: 	theString - the string to be split
    * 		pos - the position at which to split
    *            resList - an array of strings in which to place results
    *****************************************************/
   // replace this line with a definition for splitter
-
+  public static String splitter(String theString, int position, String[] resultList){
+    theString.substrings(0, position);
+  }
 
 }
