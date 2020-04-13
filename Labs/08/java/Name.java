82c734eaf5232d1880261584d157561123b9d1af
AJ Vrieland <zer0relm@gmail.com>
2020-04-13 16:42:23 -0400

    Lab08 java no comments

--- Labs/08/java/Name.java
+++
@@ -1,7 +0,0 @@
-/* Name.java implements class Name.
- * Begun by: Prof. Adams, for CS 214 at Calvin College.
- * Student:
- * Date:
- ************************************************************/
-
-
