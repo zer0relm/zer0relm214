93a69726e1b498b2791629d20d64ac95e65b17ba
AJ Vrieland <zer0relm@gmail.com>
2020-04-14 10:25:35 -0400

    Lab08 Ada

--- Labs/Lab08/ada/name_package.ads
+++ Labs/Lab08/ada/name_package.ads
@@ -1,7 +1,27 @@
 -- name_package.ads declares the Name type and its operations.
 --
 -- Begun by: Dr. Adams, CS 214 at Calvin College.
--- Completed by:
--- Date:
+-- Completed by: AJ Vrieland (ajv234)
+-- Date: 04/14/2020
 --------------------------------------------------------------
 
+package name_package is
+  type Name is private;
+
+  procedure Init(Nm: out Name; First, Middle, Last : in String);
+  function getFirst( Nm: in Name) return String;
+  function getMiddle( Nm: in Name) return String;
+  function getLast( Nm: in Name) return String;
+  function getFullName( Nm: in Name) return String;
+  procedure Put(Nm: in Name);
+
+
+
+  private NAME_SIZE : constant Positive := 8;
+  type Name is
+    record
+      myFirst,
+      myMiddle,
+      MyLast : String(1..NAME_SIZE);
+    end record;
+end name_package;
