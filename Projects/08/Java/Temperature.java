4406eb4051d207cf08f008c5fce595cabb74e167
AJ Vrieland <zer0relm@gmail.com>
2020-04-23 19:07:02 -0400

    Project08 Java progress

--- Projects/08/Java/Temperature.java
+++ Projects/08/Java/Temperature.java
@@ -5,47 +5,118 @@
 class Temperature
 {
 
-  public Temperature(final double degree, final String scale){
+  public Temperature(double degree, String scale){
     myDegree = degree;
-    myScale = scale;
-    assert scale == "K" || scale == "F" || scale == "f" || scale == "k" ||
-      scale == "C" || scale == "c";
+    myScale = scale.charAt(0);
   }
 
-  public final double getFahrenheit(){
-    if(myScale != "F" || myScale != "f"){
-      if(myScale == "C" || myScale == "c"){
-        return (myDegree * (1.8)) + 32;
-      }else if(myScale == "K" || myScale == "k"){
-        return myDegree * (1.8) - 459.67;
-    }
-    }
+  private boolean isValid(){
+    assert myScale == 'K' || myScale == 'F' || myScale == 'f' || myScale == 'k' ||
+      myScale == 'C' || myScale == 'c';
+    return true;
+  }
+
+  public double getDegree(){
     return myDegree;
   }
-
-  public final double getCelsius(){
-    if(myScale != "C" || myScale != "c"){
-      if(myScale == "F" || myScale == "f"){
-        return (myDegree - 32)/(1.8);
-      } else if(myScale == "K" || myScale == "k"){
-      return myDegree - 273.15;
+  public char getScale(){
+    return myScale;
+  }
+  /* getFahrenheit/Celsius/Kelvin() converts the temperature to given scale
+   * Recieve: a Temperature in one of the three scales
+   * Output: The converted temperature
+   **************************************************************************/
+  public void toFahrenheit(){
+    if(myScale != 'F' || myScale != 'f'){
+      if(myScale == 'C' || myScale == 'c'){
+        myDegree = (myDegree * (1.8)) + 32;
+        myScale = 'F';
+      }else if(myScale == 'K' || myScale == 'k'){
+        myDegree = myDegree * (1.8) - 459.67;
+        myScale = 'F';
     }
     }
-    return myDegree;
   }
 
-  public final double getKelvin(){
-    if(myScale != "K" || myScale != "k"){
-      if(myScale == "F" || myScale == "f"){
-        return (myDegree + 459.67)/1.8;
-      } else if(myScale == "C" || myScale == "c"){
-        return myDegree + 273.15;
+  public void toCelsius(){
+    if(myScale != 'C' || myScale != 'c'){
+      if(myScale == 'F' || myScale == 'f'){
+        myDegree = (myDegree - 32)/(1.8);
+        myScale = 'C';
+      } else if(myScale == 'K' || myScale == 'k'){
+      myDegree = myDegree - 273.15;
+      myScale = 'C';
+    }
+    }
+  }
+
+  public void toKelvin(){
+    if(myScale != 'K' || myScale != 'k'){
+      if(myScale == 'F' || myScale == 'f'){
+        myDegree = (myDegree + 459.67)/1.8;
+        myScale = 'K';
+      } else if(myScale == 'C' || myScale == 'c'){
+        myDegree = myDegree + 273.15;
+        myScale = 'K';
       }
     }
-    return myDegree;
   }
 
 
-  private Double myDegree;
-  private String myScale;
+  /* setTemperature() given a string with a temperature and scale will
+   *      update Temperature to that new value and scale
+   * Recieve: a String containing a degree and scale
+   * Output: the Temperature is updated
+   **********************************************************************/
+  public void setTemperature(String temp){
+    String tempDegree = "";
+    char tempScale = ' ';
+    char tempChar;
+    for(int i = 0; i < temp.length(); i++){
+      tempChar = temp.charAt(i);
+      if(Character.isDigit(tempChar) || tempChar == '.'){
+        tempDegree += tempChar;
+      } else if(tempChar == ' '){
+        continue;
+      } else if (Character.isLetter(tempChar)){
+        tempScale = tempChar;
+      }
+    }
+
+    myDegree = Double.valueOf(tempDegree);
+    myScale = tempScale;
+  }
+
+  /* toString() returns a string version of the Temperature
+  */
+  public String toString(){
+    return Double.toString(myDegree) + " " + myScale;
+  }
+
+  public void raise(double stepVal){
+    myDegree += stepVal;
+  }
+
+  public void lower(double stepVal){
+    myDegree -= stepVal;
+  }
+
+
+
+  public boolean equals(Temperature temp2){
+    if(myScale == 'C' || myScale == 'c'){
+      temp2.toCelsius();
+    } else if(myScale == 'F' || myScale == 'f'){
+      temp2.toFahrenheit();
+    } else if(myScale == 'K' || myScale == 'k'){
+      temp2.toKelvin();
+    }
+    if(myDegree == temp2.getDegree()){
+      return true;
+    }
+    return false;
+  }
+
+  private double myDegree;
+  private char myScale;
 }
--- Projects/08/Java/TemperatureTest.java
+++ Projects/08/Java/TemperatureTest.java
@@ -1,28 +1,22 @@
 
+import java.util.Scanner;
+
 public class TemperatureTest{
   public static void main(String[] args) {
-    Temperature kelvin = new Temperature(500, "K");
-    Temperature celsius = new Temperature(0, "c");
-    Temperature fahrenheit = new Temperature(75, "F");
+    Scanner keyboard = new Scanner(System.in);
+    Temperature temp1 = new Temperature(0.0, "f");
+    Temperature temp2 = new Temperature(0.0, "F");
 
-    Temperature rankine =  new Temperature(491.67, "R");
+    Temperature test = new Temperature(0.0, "c");
+    //System.out.print(temp2.getDegree());
+    System.out.print(temp1.equals(test));
+    test.setTemperature("-17.778 c");
+    System.out.print(test);
+    System.out.print(temp1.equals(test));
+    System.out.print(test);
 
-    // assert celsius.getFahrenheit() == 32 : "C->F";
-    // //System.out.print(celsius.getFahrenheit());
-    // assert kelvin.getFahrenheit() == 440.33 : "K->F";
-    // //System.out.print(kelvin.getFahrenheit());
-    // assert fahrenheit.getFahrenheit() == 75 : "F->F";
-    //
-    // System.out.print(kelvin.getCelsius());
-    // assert kelvin.getCelsius() == 226.85 : "K->C";
-    // assert celsius.getCelsius() == 0 : "C->C";
-    // System.out.print(fahrenheit.getCelsius());
-    // assert fahrenheit.getCelsius() == 23.89 : "F->C";
-
-    // System.out.printf("0 C in K is %f\n", celsius.getKelvin());
-    // System.out.printf("75 F in K is %f\n", fahrenheit.getKelvin());
-    // System.out.printf("500 in K is %f\n", kelvin.getKelvin());
-
-
+    // System.out.print("Enter base Temp: ");
+    // String temp = keyboard.next();
+    // temp1.setTemperature(temp);
   }
 }
